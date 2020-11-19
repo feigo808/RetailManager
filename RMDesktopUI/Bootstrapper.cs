4638e1f7e3f37f93ecb68e441b18352ca4e5eac3
Fei <fei@reseller.visualhawaii.com>
2020-11-18 17:53:18 -1000

    Add WPF project with MVVM
    Adds out WPF iser interface project and configures it with Caliburn Micro for our MVVM framework. Also includes the Simple container DI system.

---
+++ RMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,67 @@
+﻿using Caliburn.Micro;
+using RMDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMDesktopUI
+{ 
+    public class Bootstrapper : BootstrapperBase
+    {
+        // It's gonna to handle the instantiation of our classes
+        private SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            // Whenever you ask for a simple container instance they'll return this instance to you
+            _container.Instance(_container);
+            // Singleton: We create one instance of the class for the life of application
+            // Then if the shellViewModel asks for an IEventAggregator, it's gonna get back the first EventAggregator ever created 
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            // Get all the classes end with ViewModel, then create a list, for every ViewModel class, link the view and the viewModel
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        /// <summary>
+        /// When we pass a type and a name, then get the insatance
+        /// </summary>
+        /// <param name="service"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
